17619a701ad20b88f73086c9e95a7e4f85e78277
rafaelreisramos <rrramos@gmail.com>
2022-10-17 15:46:15 -0300

    fix: allow user classNames

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,18 +6,23 @@ export type TextProps = {
   size?: 'sm' | 'md' | 'lg'
   children: ReactNode
   asChild?: boolean
+  className?: string
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const ModifiedTextComponent = asChild ? Slot : 'span'
 
   return (
     <ModifiedTextComponent
-      className={clsx('text-gray-100 font-sans', {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        },
+        className
+      )}
     >
       {children}
     </ModifiedTextComponent>
