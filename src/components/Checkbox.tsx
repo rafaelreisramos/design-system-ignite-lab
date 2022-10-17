ab089273fd11ecd6e251017cdf51ee677a6f1dad
rafaelreisramos <rrramos@gmail.com>
2022-10-17 15:44:56 -0300

    fix: extend checkbox props from Radix

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,10 +1,14 @@
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
-export type CheckboxProps = {}
 
-export function Checkbox({}: CheckboxProps) {
+export type CheckboxProps = CheckboxRadix.CheckboxProps
+
+export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxRadix.Root className="w-6 h-6 p-[2px] rounded bg-gray-800 ">
+    <CheckboxRadix.Root
+      className="w-6 h-6 p-[2px] rounded bg-gray-800}"
+      {...props}
+    >
       <CheckboxRadix.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
       </CheckboxRadix.Indicator>
